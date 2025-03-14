26b89df4ff81abb31b2b6f205854ccbdefef97c2
Erik P Almaraz <erikalmaraz@fastmail.com>
2025-03-13 17:32:06 -0700

    Closes #2

--- config/system/core/heizenberg.scm
+++ config/system/core/heizenberg.scm
@@ -1,4 +1,4 @@
-(define-module (config system core locutus)
+(define-module (config system core heizenberg)
   #:use-module (gnu)
   #:use-module (gnu packages)
   #:use-module (gnu services)
@@ -6,7 +6,7 @@
   #:use-module (guix transformations)
   #:use-module (config system core base-system)
 
-  #:export (%locutus))
+  #:export (%heizenberg))
 
 (use-system-modules keyboard)
 
@@ -35,18 +35,18 @@
 (set! guixos-swap-devices
   (list (swap-space
          (target
-          (uuid "08b16346-aa6b-4744-9856-6070d78e38ca")))))
+          (uuid "0bf14f0b-3a99-439c-8ee0-130ce0792694")))))
 
 (set! guixos-file-systems
   ;; Use 'blkid' to find unique file system identifiers ("UUIDs").
   (cons* (file-system
           (mount-point  "/boot/efi")
-          (device (uuid "F8E9-9C22"
+          (device (uuid "645B-9C47"
 			'fat32))
           (type "vfat"))
          (file-system
           (mount-point "/")
-          (device (uuid "872f93f3-bcb4-4907-bb4c-d6002496d3a8"
+          (device (uuid "3d82108c-2c00-4238-b775-9f0c8894a638"
 			'ext4))
           (type "ext4"))
 	 %base-file-systems))
@@ -57,21 +57,22 @@
    (user-group (system? #t) (name "seat"))
    %base-groups))
 
+;; TODO: treat as constant and add this user to list of "guixos-base-users"
 (set! guixos-users
-  (cons* (user-account
-          (name "logoraz")
-          (comment "Worker Bee")
-          (home-directory "/home/logoraz")
-          (group "users")
-          (supplementary-groups '("wheel"    ;; sudo
-                                  "seat"     ;; -
-                                  "netdev"   ;; network devices
-                                  "tty"      ;; -
-                                  "input"    ;; -
-                                  "lp"       ;; control bluetooth devices
-                                  "audio"    ;; control audio devices
-                                  "video"))) ;; control video devices
-         %base-user-accounts))
+      (cons* (user-account
+              (name "logoraz")
+              (comment "Worker Bee")
+              (home-directory "/home/logoraz")
+              (group "users")
+              (supplementary-groups '("wheel" ;; sudo
+                                      "seat"  ;; -
+                                      "netdev" ;; network devices
+                                      "tty"    ;; -
+                                      "input"  ;; -
+                                      "lp"     ;; control bluetooth devices
+                                      "audio"  ;; control audio devices
+                                      "video"))) ;; control video devices
+             %base-user-accounts))
 
 
 ;;; Packages & Transformations
@@ -108,10 +109,10 @@
 
 ;;; Define GuixOS StumpWm - Crystallized Momentum
 
-(define %locutus
+(define %heizenberg
   (operating-system
    (inherit %guixos-base-system)
-   (host-name "locutus")
+   (host-name "heizenberg")
    (timezone "America/Los_Angeles")
    (locale "en_US.utf8")
    (keyboard-layout guixos-keyboard-layout)
